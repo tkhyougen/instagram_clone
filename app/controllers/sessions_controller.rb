837704a3ff382d823e8c10eba0be6627a08ae6b7
tomohiko <illgalsubstance@yahoo.co.jp>
2020-02-02 07:13:17 +0000

    css修正 コンパイル

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = "ログインに失敗しました"
+      flash.now[:notice] = "ログインに失敗しました"
       render "new"
     end
   end
 
   def destroy
     session.delete(:user_id)
-    # flash[:notice]="ログアウトしました"
+     flash[:notice]= "ログアウトしました"
     redirect_to new_session_path
   end
 end
